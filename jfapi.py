53092d623786cf366703636d5c2d72c2efee80da
felix920506 <felix920506@gmail.com>
2024-09-21 15:25:55 -0400

    play single tracks by id

---
+++ jfapi.py
@@ -0,0 +1,123 @@
+import aiohttp
+import asyncio
+import json
+import yaml
+import urllib.parse
+
+class JFAPI():
+    def __init__(self, server: str, apikey: str) -> None:
+        self.__apikey = apikey
+        self.__server = server.strip('/')
+        self._session = None
+
+    def __getEndpointUrl(self, endpoint: str):
+        return f'{self.__server}/{endpoint.strip('/')}'
+
+    async def __aenter__(self):
+        self._session = aiohttp.ClientSession()
+    
+    async def __aexit__(self):
+        await self._session.close()
+    
+    def __del__(self):
+        try:
+            asyncio.run(self._session.delete())
+        except:
+            pass
+
+    async def checkSession(self):
+        if not self._session:
+            self._session = aiohttp.ClientSession()
+
+    # https://api.jellyfin.org/#tag/Search/operation/GetSearchHints
+    # GET /Search/Hints
+    async def search(self, term: str, limit: int = None, types:list[str] = []) -> list:
+        await self.checkSession()
+        params = {
+            'ApiKey': self.__apikey,
+            'searchTerm': term,
+        }
+        if limit:
+            params['limit'] = limit
+        if types:
+            params['includeItemTypes'] = ','.join(types)
+
+        async with self._session.get(
+            self.__getEndpointUrl('/Search/Hints'),
+            params=params
+        ) as res:
+            return (await res.json())["SearchHints"]
+
+    # Gets HLS stream for audio soundtrack with format Opus 16bit 48khz in fMP4 containers
+    # Returns URL for use with external player (ffmpeg)
+    # GET /Audio/{itemId}/main.m3u8
+    def getAudioHls(self, id, bitrate):
+        endpoint = self.__getEndpointUrl(f'/Audio/{id}/main.m3u8')
+        params = {
+            'ApiKey': self.__apikey,
+            'segmentContainer': 'mp4',
+            'audioCodec': 'opus',
+            'allowAudioStreamCopy': True,
+            'maxAudioBitDepth': 16,
+            'audioSampleRate': 48000,
+            'audioChannels': 2,
+            'audioBitRate': bitrate
+        }
+        q = urllib.parse.urlencode(params)
+        return endpoint + '?' + q
+
+    # Gets items by IDs
+    # GET /Items
+    async def getItemsByIds(self, ids: list[str]):
+        await self.checkSession()
+        endpoint = self.__getEndpointUrl('/Items')
+        params = {
+            'ApiKey': self.__apikey,
+            'ids': ','.join(ids)
+        }
+        async with self._session.get(endpoint, params=params) as res:
+            return (await res.json())['Items']
+
+'''
+Data Classes
+'''
+class Album:
+    def __init__(self):
+        pass
+
+    @property
+    def tracks(self) -> str:
+        pass
+
+    
+
+class Track:
+    def __init__(self):
+        # empty track with no data
+        self.__init__('', '', 0, [])
+    
+    def __init__(self, id: str, title: str, length: int, artists: list[str] | None = []):
+        self._artists = artists
+        self._title = title
+        self._length = length
+        self._id = id
+    
+    def __init__(self, queryResult: dict):
+        self.__init__(
+            queryResult['Id'],
+            queryResult['Name'],
+            queryResult['RunTimeTicks'] // 10000000,
+            queryResult['Artists']
+        )
+
+    @property
+    def length(self) -> int:
+        return self._length
+    
+    @property
+    def title(self) -> str:
+        return self._title
+    
+    @property
+    def id(self) -> str:
+        return self._id
--- main.py
+++ main.py
@@ -1,7 +1,135 @@
 import discord
+import discord.ext
 import asyncio
 import yaml
 
-with open('config.yml', 'r', encoding='utf8') as conffile:
-    config = yaml.load(conffile, yaml.loader())
+from jfapi import JFAPI
 
+with open('config.yml', 'r', encoding='utf8') as conffile:
+    config = yaml.load(conffile, yaml.loader.Loader)
+
+JF_APICLIENT = JFAPI(config['jf-server'],config['jf-apikey'])
+LIMIT = max(1, min(config['search-limit'], 9))
+
+queues = {}
+playing = {}
+
+bot = discord.Bot()
+
+'''
+Data Classes
+'''
+
+
+'''
+Helper Functions
+'''
+async def searchHelper(term: str, limit: int = LIMIT, type:str = None):
+    if type == 'Soundtrack':
+        type = ['Audio']
+    elif type == 'Album':
+        type = ['MusicAlbum']
+    else:
+        type = ['Audio', 'MusicAlbum']
+    
+    res = await JF_APICLIENT.search(term, limit, type)
+    return res
+
+async def playHelperTrack(item: dict, ctx: discord.ApplicationContext, position: str):
+    # url = JF_APICLIENT.getAudioHls(item["Id"])
+    entry = {
+        "Artists": item["Artists"],
+        "Title": item["Name"],
+        "Id": item['Id']
+    }
+    global queues
+    # TODO: properly do position in queue
+    if not ctx.guild_id in queues:
+        queues[ctx.guild_id] = []
+    queues[ctx.guild_id].append(entry)
+
+    if not ctx.voice_client:
+        await startPlayer(ctx)
+
+
+async def playHelperAlbum(item: str, guild: int | discord.Guild):
+    pass
+
+
+async def startPlayer(ctx: discord.ApplicationContext):
+    vc = ctx.voice_client
+    if not vc:
+        av = ctx.author.voice
+        if not av:
+            ctx.respond('You are not in any voice channel')
+            return
+        else:
+            vc = await av.channel.connect()
+            await playTrack(ctx.guild)
+
+async def playTrack(guild: discord.Guild):
+    vc = guild.voice_client
+    if vc.paused:
+        vc.resume()
+    else:
+        await asyncio.to_thread(playNextTrack, guild)
+        # br = vc.channel.bitrate
+        # global playing
+        # global queues
+        # playing[guild.id] = queues[guild.id].pop(0)
+        # url = JF_APICLIENT.getAudioHls(playing[guild.id]["Id"],br)
+        # audio = discord.FFmpegOpusAudio(url, codec='copy')
+        # await asyncio.to_thread(audio.read())
+        # vc.play(audio, after=lambda e: await playNextTrack(guild, e))
+
+def playNextTrack(guild, error=None):
+    vc = guild.voice_client
+    br = vc.channel.bitrate
+    global playing
+    global queues
+    playing[guild.id] = queues[guild.id].pop(0)
+    url = JF_APICLIENT.getAudioHls(playing[guild.id]["Id"],br)
+    # use libopus until py-cord 2.7
+    # change to 'copy' after py-cord 2.7 is out
+    audio = discord.FFmpegOpusAudio(url, codec='libopus')
+    audio.read()
+    vc.play(audio, after=lambda e: playNextTrack(guild, e))
+
+
+'''
+Bot Commands
+'''
+cmdgrp = bot.create_group(config['command-group'], guild_ids=[969479656069804063])
+
+@cmdgrp.command()
+async def search(ctx: discord.ApplicationContext, 
+                 term: discord.Option(str),
+                 type: discord.Option(str, choices=['Soundtrack', 'Album'], required=False)):
+    
+    res = await searchHelper(term, type=type)
+    entries = [f'{i["Artists"][0]} - {i["Name"]}' for i in res]
+    await ctx.respond('\n'.join(entries))
+
+@cmdgrp.command()
+async def play(ctx: discord.ApplicationContext,
+               term: discord.Option(str),
+               type: discord.Option(str, choices=['Soundtrack', 'Album'], required=False),
+               when: discord.Option(str, choices=['now', 'next', 'last'], required=False)):
+
+    res = await searchHelper(term, limit=1, type=type)
+
+@cmdgrp.command()
+async def playbyid(ctx: discord.ApplicationContext,
+                   id: discord.Option(str),
+                   when: discord.Option(str, choices=['now', 'next', 'last'], required=False)):
+    items = await JF_APICLIENT.getItemsByIds([id])
+    if not items:
+        await ctx.respond('ID does not exist')
+    else:
+        await ctx.respond('Playing item')
+        await playHelperTrack(items[0], ctx, 0)
+        
+
+
+
+bot.run(config['discord-token'])
